4322d4c260e892fed9c29be18ad59bc98c198304
AZAMAT YADYGEROV <73704375+azamat1696@users.noreply.github.com>
2024-09-06 14:01:50 +0300

    Compoent partial is done

---
+++ RealEstateUI/ViewComponents/HomePage/_DefaultFooterComponentPartial.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstateUI.ViewComponents.HomePage;
+
+public class _DefaultFooterComponentPartial : ViewComponent
+{
+     public IViewComponentResult Invoke()
+     {
+         return View();
+     }
+}
---
+++ RealEstateUI/ViewComponents/HomePage/_DefaultOurClientsComponentPartial.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace RealEstateUI.ViewComponents.HomePage;
+
+public class _DefaultOurClientsComponentPartial : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        return View();
+    }
+}
